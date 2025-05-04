7bfa392253fdad72def06207ee49ad6193aaf128
Yana Medvedeva <134713030+Tosh1ii@users.noreply.github.com>
2025-05-04 23:01:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,58 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  void push(const T& val) {
+    Node* newNode = new Node(val);
+    if (!head || val.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next && current->next->data.prior >= val.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+
+T pop() {
+  if (!head) throw std::runtime_error("Queue is empty");
+  Node* temp = head;
+  T result = temp->data;
+  head = head->next;
+  delete temp;
+  return result;
+}
+
+  const T& front() const {
+    if (!head) throw std::runtime_error("Queue is empty");
+    return head->data;
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+
+ private:
+  struct Node {
+    explicit Node(const T& val) : data(val), next(nullptr) {}
+    T data;
+    Node* next;
+  };
+
+  Node* head;
 };
 
 struct SYM {
